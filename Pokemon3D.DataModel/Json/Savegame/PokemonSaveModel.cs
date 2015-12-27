1b91dcb7d241f9482dc4ab1da54cd3792b824830
nilllzz <nidresch@googlemail.com>
2015-12-27 04:45:09 +0100

    Fixed missing data members

--- Pokemon3D.DataModel/Json/Savegame/PokemonSaveModel.cs
+++ Pokemon3D.DataModel/Json/Savegame/PokemonSaveModel.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Runtime.Serialization;
+﻿using System.Runtime.Serialization;
 using Pokemon3D.DataModel.Json.Pokemon;
 
 // Disable Code Analysis for warning CS0649: Field is never assigned to, and will always have its default value.
@@ -10,8 +9,10 @@ namespace Pokemon3D.DataModel.Json.Savegame
     [DataContract]
     public class PokemonSaveModel : JsonDataModel<PokemonSaveModel>
     {
+        [DataMember(Order = 0)]
         public int Id;
 
+        [DataMember(Order = 1, Name = "Gender")]
         private string _gender;
 
         public PokemonGender Gender
@@ -20,30 +21,43 @@ namespace Pokemon3D.DataModel.Json.Savegame
             set { _gender = value.ToString(); }
         }
 
+        [DataMember(Order = 2)]
         public PokemonStatSetModel IVs;
 
+        [DataMember(Order = 3)]
         public bool IsShiny;
 
+        [DataMember(Order = 4)]
         public int AbilityId;
 
+        [DataMember(Order = 5)]
         public int NatureId;
 
+        [DataMember(Order = 6)]
         public string OT;
-        
+
+        [DataMember(Order = 7)]
         public PokemonCatchInfo CatchInfo;
 
+        [DataMember(Order = 8)]
         public string PersonalityValue;
 
+        [DataMember(Order = 9)]
         public int HP;
 
+        [DataMember(Order = 10)]
         public int Experience;
 
+        [DataMember(Order = 11)]
         public string Nickname;
 
+        [DataMember(Order = 12)]
         public int Friendship;
 
+        [DataMember(Order = 13)]
         public HeldItemModel Item;
 
+        [DataMember(Order = 14, Name = "Status")]
         private string _status;
 
         public PokemonStatus Status
@@ -52,12 +66,16 @@ namespace Pokemon3D.DataModel.Json.Savegame
             set { _status = value.ToString(); }
         }
 
+        [DataMember(Order = 15)]
         public PokemonStatSetModel EVs;
 
+        [DataMember(Order = 16)]
         public int EggSteps;
 
+        [DataMember(Order = 17)]
         public string AdditionalData;
 
+        [DataMember(Order = 18)]
         public PokemonMoveModel[] Moves;
 
         public override object Clone()
