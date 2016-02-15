5c991f4af0dea772fff03a4e61a422103af28a13
nilllzz <nidresch@googlemail.com>
2016-02-15 19:56:07 +0100

    Shape interface

---
+++ Pokemon3D.Common/Shapes/Shape.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+
+namespace Pokemon3D.Common.Shapes
+{
+    /// <summary>
+    /// A two dimensional shape.
+    /// </summary>
+    public interface Shape
+    {
+        /// <summary>
+        /// The outer bounds of this shape.
+        /// </summary>
+        Rectangle Bounds { get; }
+
+        /// <summary>
+        /// The upper left corner of this shape.
+        /// </summary>
+        Point Location { get; }
+
+        /// <summary>
+        /// Calculates the area of this shape.
+        /// </summary
+        double GetArea();
+
+        /// <summary>
+        /// Returns if this shape contains specific x and y coordinates.
+        /// </summary>
+        bool Contains(int x, int y);
+
+        /// <summary>
+        /// Returns if this shape contains a <see cref="Vector2"/>.
+        /// </summary>
+        bool Contains(Vector2 value);
+
+        /// <summary>
+        /// Returns if this shape contains a <see cref="Rectangle"/>.
+        /// </summary>
+        bool Contains(Rectangle rectangle);
+
+        /// <summary>
+        /// Returns if this shape contains a <see cref="Point"/>.
+        /// </summary>
+        bool Contains(Point value);
+
+        /// <summary>
+        /// Returns if this shape contains another <see cref="Shape"/>.
+        /// </summary>
+        bool Contains(Shape shape);
+    }
+}
