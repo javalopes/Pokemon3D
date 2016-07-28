9ef0bc7bd08592caf10966244a567d66e5d05a29
trommlbomml <peter_friedland@gmx.de>
2016-07-28 16:41:00 +0200

    Added unity scene file reader

---
+++ UnityToJsonMapConverter/CommandLineParser.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnityToJsonMapConverter
+{
+    class CommandLineParser
+    {
+        public string UnityFilePath { get; private set; }
+
+        public string JsonMapFilePath{ get; private set; }
+
+        public CommandLineParser(string[] args)
+        {
+            if (args.Length != 2)
+            {
+                throw new InvalidOperationException("Usage is:\n UnityToJsonMapConverter.exe unityscene targetjsonfile");
+            }
+
+            UnityFilePath = args[0];
+            JsonMapFilePath = args[1];
+        }
+    }
+}
