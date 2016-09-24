9083a115ddf2d47c81425dc1f569a191d28b89e4
nilllzz <nidresch@googlemail.com>
2016-09-24 17:33:54 +0200

    TypeContract functionality added to scripting project

---
+++ Pokemon3D.Scripting/TypeContract.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Pokemon3D.Scripting.Types;
+
+namespace Pokemon3D.Scripting
+{
+    internal static class TypeContract
+    {
+        public static bool Ensure(SObject[] parameters, Type typeContract)
+        {
+            return Ensure(parameters, new[] { typeContract });
+        }
+
+        public static bool Ensure(SObject[] parameters, Type[] typeContract)
+        {
+            if (parameters.Length >= typeContract.Length)
+            {
+                var i = 0;
+
+                while (i < parameters.Length)
+                {
+                    if (i < typeContract.Length && typeContract[i] != SObject.Unbox(parameters[i]).GetType())
+                        return false;
+
+                    i++;
+                }
+
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
